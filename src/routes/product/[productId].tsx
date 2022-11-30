1720e363a14f2369cf723655a2ed0e29b1e9caa7
Louis La Grange <louis@louislagrange.co.za>
2022-11-30 19:47:25 +0200

    Start structuring product info page

--- src/routes/product/[productId].tsx
+++ src/routes/product/[productId].tsx
@@ -1,11 +1,50 @@
-import { Title, useParams } from "solid-start";
-import Counter from "~/components/Counter";
+import { Show } from "solid-js";
+import { useParams } from "solid-start";
+import { GifkikkerButton } from "~/components/GifkikkerButton";
+import { GifkikkerInput } from "~/components/GifkikkerInput";
+import { PRODUCTS } from "~/const/products";
+import "./product.css";
 
 export default function Product() {
   const params: { productId: string } = useParams();
+  const productId: string = params.productId;
+  const currentProduct = PRODUCTS.get(productId);
   return (
-    <main>
-      <p>This is product with ID: {params.productId}</p>
-    </main>
+    <div class="product-content" id="product-content">
+      <Show
+        when={currentProduct}
+        fallback={<div>no product with id {productId} found</div>}
+      >
+        <div class="section">
+          <div class="description">
+            <span class="title">{currentProduct?.brand}</span>
+            <p class="text">{currentProduct?.type}</p>
+            <span class="price">
+              <a>Cake Price: €</a>
+              <a id="price">000</a>
+            </span>
+          </div>
+          <div class="order-options">
+            <GifkikkerInput
+              type="number"
+              id="amount"
+              placeholder="amount of cakes"
+              value="1"
+              min="1"
+              max="10"
+            />
+            <span class="error-msg" id="error-msg"></span>
+            <span class="total">
+              <a>Total: R</a>
+              <a id="total">--</a>
+            </span>
+            <GifkikkerButton>ORDER NOW!</GifkikkerButton>
+          </div>
+        </div>
+        <div class="cake-pic">
+          <img src={currentProduct?.imageUrl} />
+        </div>
+      </Show>
+    </div>
   );
 }
