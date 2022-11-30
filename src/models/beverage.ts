a9fd097d4501f7a74bb90151a6ef8e5a7d496471
Louis La Grange <louis@louislagrange.co.za>
2022-11-30 19:46:57 +0200

    Update product model type

--- src/models/beverage.ts
+++ src/models/beverage.ts
@@ -1,8 +1,10 @@
 export interface Beverage {
+  productId: string;
   volume: number; // centilitre
   alcoholPercentage: number;
   brand: BeverageType;
   type: BeverageBrand;
+  imageUrl: string;
 }
 
 type BeverageType = "herfstbok" | "lentebok" | "ipa" | "lager";
