99f943421d7ad949aa42a4b6f993ff8af3308ab8
Louis La Grange <louis@louislagrange.co.za>
2022-12-01 22:51:33 +0200

    Add global color vars & update styles

--- src/routes/browse.tsx
+++ src/routes/browse.tsx
@@ -1,18 +1,25 @@
 import { For } from "solid-js";
 import { A } from "solid-start";
+import { Card } from "~/components/Card";
 import { PRODUCTS } from "~/const/products";
+import "./browse.scss";
 
 export default function Browse() {
   return (
-    <div>
+    <div class="products">
       <p>hold my beer</p>
-      <For each={Array.from(PRODUCTS.values())}>
-        {(bev) => (
-          <A href={`/product/${bev.productId}`}>
-            <div>{bev.type}</div>
-          </A>
-        )}
-      </For>
+      <div class="products__container">
+        <For each={Array.from(PRODUCTS.values())}>
+          {(product) => (
+            <A href={`/product/${product.productId}`}>
+              <Card class="products__container__item">
+                <img src={product.imageUrl} />
+                <div>{product.description}</div>
+              </Card>
+            </A>
+          )}
+        </For>
+      </div>
     </div>
   );
 }
