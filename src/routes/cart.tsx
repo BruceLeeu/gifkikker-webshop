b32022892485c0e8b80625571c6f7d0e7f39230d
Louis La Grange <louis@louislagrange.co.za>
2022-12-02 19:08:03 +0200

    Add cart functionality

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -1,7 +1,61 @@
-export default function Cart() {
+import { For } from "solid-js";
+import { useNavigate } from "solid-start";
+import { GifkikkerButton } from "~/components/GifkikkerButton";
+import { StyledTable } from "~/components/StyledTable";
+import { Cart } from "~/models/product";
+import "./cart.scss";
+
+export default function CartPage() {
+  const currentCart = JSON.parse(localStorage.getItem("cart") ?? "{}") as Cart;
+  const navigate = useNavigate();
   return (
     <main>
-      <p>This is my trolley</p>
+      <StyledTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Title</th>
+            <th>Sell Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <For each={Object.entries(currentCart)}>
+            {(cartItem) => (
+              <tr
+                onClick={() =>
+                  navigate(`/product/${cartItem[1].product.productId}`)
+                }
+              >
+                <td>
+                  <div>
+                    <img src={cartItem[1].product.imageUrl} />
+                  </div>
+                  {/* <div>{cartItem[1]}</div> */}
+                </td>
+                <td>
+                  <div>{cartItem[1].product.title}</div>
+                </td>
+                <td>
+                  <div>€{cartItem[1].product.sellPrice}</div>
+                </td>
+                <td>
+                  <div>{cartItem[1].amount}</div>
+                </td>
+                <td>
+                  <div>
+                    €{cartItem[1].product.sellPrice * cartItem[1].amount}
+                  </div>
+                </td>
+              </tr>
+            )}
+          </For>
+        </tbody>
+      </StyledTable>
+      <div class="button-container">
+        <GifkikkerButton>Continue to shipping →</GifkikkerButton>
+      </div>
     </main>
   );
 }
--- src/routes/product/[productId].tsx
+++ src/routes/product/[productId].tsx
@@ -1,22 +1,18 @@
-import { createEffect, createSignal, For, Show } from "solid-js";
+import { createSignal, For, Show } from "solid-js";
 import { useParams } from "solid-start";
 import { Card } from "~/components/Card";
 import { GifkikkerButton } from "~/components/GifkikkerButton";
 import { GifkikkerInput } from "~/components/GifkikkerInput";
 import { PRODUCTS } from "~/const/products";
+import { Cart, CartItem, Product } from "~/models/product";
 import "./product.scss";
 
-export default function Product() {
+export default function ProductInfo() {
   const params: { productId: string } = useParams();
   const productId: string = params.productId;
   const currentProduct = PRODUCTS.get(productId);
-  // const currentProductMakeup = currentProduct?.makeup;
 
   const [orderAmount, setProductAmount] = createSignal(0);
-  createEffect(() => {
-    console.log(orderAmount());
-    console.log(orderAmount() === 0);
-  });
 
   return (
     <div class="product-detail-base" id="product-content">
@@ -34,7 +30,7 @@ export default function Product() {
             <span class="title">{currentProduct?.title}</span>
             <p class="text">{currentProduct?.description}</p>
             <p class="text">
-              {currentProduct?.stock ?? 0 /*  - orderAmount() */} in stock
+              {(currentProduct?.stock ?? 0) - orderAmount()} in stock
             </p>
             <p class="text">Includes:</p>
             <For
@@ -52,10 +48,8 @@ export default function Product() {
               )}
             </For>
             <span class="price">
-              <a>Final Price: €</a>
-              <a id="price">
-                {currentProduct?.sellPrice ?? 0 /*  * orderAmount() */}
-              </a>
+              <a>Product Price: €</a>
+              <a id="price">{currentProduct?.sellPrice ?? 0}</a>
             </span>
           </Card>
           <Card>
@@ -65,7 +59,6 @@ export default function Product() {
               placeholder="amount of items"
               value={orderAmount()}
               min="0"
-              disabled={orderAmount() === 3}
               onChange={(v) => {
                 setProductAmount(v.currentTarget.valueAsNumber);
               }}
@@ -74,9 +67,19 @@ export default function Product() {
             <span class="error-msg" id="error-msg"></span>
             <span class="total">
               <a>Total: R</a>
-              <a id="total">{orderAmount()}</a>
+              <a id="total">
+                {(currentProduct?.sellPrice ?? 0) * orderAmount()}
+              </a>
             </span>
-            <GifkikkerButton disabled={orderAmount() === 0}>
+            <GifkikkerButton
+              disabled={orderAmount() === 0}
+              onClick={() =>
+                addToCart(productId, {
+                  amount: orderAmount(),
+                  product: currentProduct as Product,
+                })
+              }
+            >
               ORDER {orderAmount()} NOW!
             </GifkikkerButton>
           </Card>
@@ -84,4 +87,10 @@ export default function Product() {
       </Show>
     </div>
   );
+}
+
+function addToCart(productId: string, cartItem: CartItem) {
+  const currentCart = JSON.parse(localStorage.getItem("cart") ?? "{}") as Cart;
+  const cart: Cart = { ...currentCart, [productId]: cartItem };
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
