31e6d2debefbdffc5e1c8a357f4106ec5b8e3a8a
Louis La Grange <louis@louislagrange.co.za>
2022-12-04 00:38:51 +0200

    Add register page

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,20 +1,28 @@
-import { createSignal, For, onMount } from "solid-js";
-import { Portal } from "solid-js/web";
+import { createEffect, createSignal, onMount } from "solid-js";
 import { useNavigate } from "solid-start";
-import { Card } from "~/components/Card";
 import { GifkikkerButton } from "~/components/GifkikkerButton";
 import { GifkikkerInput } from "~/components/GifkikkerInput";
-import { Modal } from "~/components/Modal";
-import { StyledTable } from "~/components/StyledTable";
 import { Cart } from "~/models/product";
-import { getCalculatedCartTotal } from "~/utils/calculations";
-import "./cart.scss";
+import { User } from "~/models/user";
+import "./register.scss";
 
 export default function RegisterPage() {
   const [currentCart, setCurrentCart] = createSignal<Cart>(
     JSON.parse("{}") as Cart
   );
-  const [email, setEmail] = createSignal<string>();
+  const emptyUser: User = {
+    address: {
+      city: "",
+      country: "",
+      postalCode: "",
+      province: "",
+      street: "",
+    },
+    email: "",
+    name: "",
+  };
+  // `equals: false` allows the object to be updated granularly
+  const [user, setUser] = createSignal<User>(emptyUser, { equals: false });
 
   const navigate = useNavigate();
   const [showModal, toggleModal] = createSignal(false);
@@ -23,18 +31,112 @@ export default function RegisterPage() {
     setCurrentCart(JSON.parse(localStorage.getItem("cart") ?? "{}") as Cart);
   });
 
+  createEffect(() => {
+    console.log(user());
+  });
+
+  const updateUser = (
+    e: InputEvent & {
+      currentTarget: HTMLInputElement;
+      target: Element;
+    }
+  ) => {
+    switch (e.currentTarget.id) {
+      case "email":
+        setUser((u) => {
+          u.email = e.currentTarget.value;
+          return u;
+        });
+        break;
+      case "name":
+        setUser((u) => {
+          u.name = e.currentTarget.value;
+          return u;
+        });
+        break;
+      case "streetAddress":
+        setUser((u) => {
+          u.address.street = e.currentTarget.value;
+          return u;
+        });
+        break;
+      case "city":
+        setUser((u) => {
+          u.address.city = e.currentTarget.value;
+          return u;
+        });
+        break;
+      case "province":
+        setUser((u) => {
+          u.address.province = e.currentTarget.value;
+          return u;
+        });
+        break;
+      case "country":
+        setUser((u) => {
+          u.address.country = e.currentTarget.value;
+          return u;
+        });
+        break;
+      case "postalCode":
+        setUser((u) => {
+          u.address.postalCode = e.currentTarget.value;
+          return u;
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <main>
       <h1>Register your user here</h1>
-      <GifkikkerInput
-        placeholder="email"
-        onInput={(e) => setEmail(e.currentTarget.value)}
-      />
-      <GifkikkerInput placeholder="name" />
-      <GifkikkerInput placeholder="address" />
+      <h3>Personal Details</h3>
+      <div class="details__personal">
+        <GifkikkerInput
+          id="email"
+          placeholder="email"
+          type="email"
+          value={user().email}
+          onInput={updateUser}
+        />
+        <GifkikkerInput
+          id="name"
+          placeholder="name"
+          value={user().name}
+          onInput={updateUser}
+        />
+      </div>
+      <h3>Shipping Address</h3>
+      <div class="details__address">
+        <GifkikkerInput
+          id="streetAddress"
+          placeholder="Street address"
+          onInput={updateUser}
+        />
+        <GifkikkerInput id="city" placeholder="City" onInput={updateUser} />
+        <GifkikkerInput
+          id="province"
+          placeholder="State/Province"
+          onInput={updateUser}
+        />
+        <GifkikkerInput
+          id="country"
+          placeholder="Country"
+          onInput={updateUser}
+        />
+        <GifkikkerInput
+          id="postalCode"
+          placeholder="Postal Code"
+          onInput={updateUser}
+        />
+      </div>
       <GifkikkerButton
         onClick={() => {
-          navigate(`/order?email=${email()}`);
+          localStorage.setItem("user", JSON.stringify(user()));
+          navigate(`/order?email=${user()?.email}`);
         }}
       >
         Register
