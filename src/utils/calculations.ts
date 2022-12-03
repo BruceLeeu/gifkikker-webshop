f8b0030cab03696428fb6241f51a5e6a02da2724
Louis La Grange <louis@louislagrange.co.za>
2022-12-03 12:09:36 +0200

    Add cart totals util

---
+++ src/utils/calculations.ts
@@ -0,0 +1,16 @@
+import { createSignal } from "solid-js";
+import { Cart } from "~/models/product";
+
+export function getCalculatedCartTotal(cart: Cart) {
+  const [totalItems, setTotalItems] = createSignal(0);
+  const [totalCost, setTotalCost] = createSignal(0);
+
+  if (cart) {
+    for (const cartItem of Object.values(cart)) {
+      const itemTotal = cartItem.amount * cartItem.product.sellPrice;
+      setTotalCost((tc) => tc + itemTotal);
+      setTotalItems((ti) => ti + 1);
+    }
+  }
+  return [totalItems(), totalCost()];
+}
