9e820f1ce0cb2163e063fc2bc13250ff4185d1c4
Louis La Grange <louis@louislagrange.co.za>
2022-12-04 12:34:33 +0200

    Add special support for radio button inputs

--- src/components/GifkikkerInput.tsx
+++ src/components/GifkikkerInput.tsx
@@ -1,4 +1,4 @@
-import { Component, JSX } from "solid-js";
+import { Component, createUniqueId, JSX } from "solid-js";
 import "./GifkikkerInput.scss";
 
 interface GifkikkerInput extends JSX.InputHTMLAttributes<HTMLInputElement> {
@@ -6,10 +6,18 @@ interface GifkikkerInput extends JSX.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const GifkikkerInput: Component<GifkikkerInput> = (props) => {
+  const uid = createUniqueId();
   return (
     <div class="input-group">
-      <input class="input-group__box" {...props} placeholder={""} />
-      <span class="input-group__container">{props.placeholder}</span>
+      <input
+        class="input-group__box"
+        {...props}
+        placeholder={""}
+        id={props.id ?? uid}
+      />
+      <label for={uid} class="input-group__container" itemRef={props.type}>
+        {props.placeholder}
+      </label>
     </div>
   );
 };
